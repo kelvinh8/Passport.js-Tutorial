bb6a36d16422edb5d7770a5ab14528e00a24bcaf
kelvin <kelvinherianto8@gmail.com>
2020-02-25 18:07:47 +0700

    Serialize and Deserialize and cookie session

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ const express = require("express");
 const ejs = require("ejs");
 const authRoutes = require("./routes/auth-routes.js");
 const app = express();
+const passport = require("passport");
 const passportSetup = require("./config/passport.js");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
 
+app.use(cookieSession({
+  maxAge:24 * 60 * 60 * 1000,
+  keys:[process.env.MY_KEY]
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.set("view engine","ejs");
 app.use("/auth",authRoutes);
 mongoose.connect("mongodb://localhost:27017/userDB2",{useUnifiedTopology:true,useNewUrlParser:true});
--- config/passport.js
+++ config/passport.js
@@ -3,6 +3,15 @@ const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const User = require("../models/user-model.js");
 
+passport.serializeUser((user,done)=>{
+  done(null,user.id);
+})
+passport.deserializeUser((id,done)=>{
+  User.findById(id,(err,user)=>{
+    done(null,user);
+  })
+})
+
 passport.use(new GoogleStrategy({
   callbackURL:"/auth/google/redirect",
   clientID:process.env.CLIENT_ID,
@@ -10,13 +19,15 @@ passport.use(new GoogleStrategy({
 },(accessToken,refreshToken,profile,done)=>{
   User.findOne({googleId:profile.id},(err,currentUser)=>{
     if(currentUser){
-      console.log(`found user: ${currentUser}`)
+      console.log(`found user: ${currentUser}`);
+      done(null,currentUser);
     }else{
       new User({
         name:profile.displayName,
         googleId:profile.id
       }).save().then((newUser)=>{
         console.log(`new user created:${newUser}`);
+        done(null,newUser);
       })
     }
   })
